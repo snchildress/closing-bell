b7a1fa4cdb3b280a74e658b092a0685e2cb5fb4c
snchildress <steven.n.childress@gmail.com>
2018-03-10 14:23:37 -0600

    Add initial settings URL patterns

--- settings/urls.py
+++ settings/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+
+urlpatterns = [
+    path('user/<int:id>', views.display_user_settings),
+    path('users/all', views.display_all_users_settings),
+]
---
+++ settings/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+
+def display_user_settings(request, id):
+    return HttpResponse('Placeholder')
+
+def display_all_users_settings(request):
+    return HttpResponse('Placeholder')
