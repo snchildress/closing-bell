cf34d982cb808a7dd12f821487b9847fe802d4e5
snchildress <steven.n.childress@gmail.com>
2018-03-10 21:12:49 -0600

    Make link to user settings page functional

--- settings/urls.py
+++ settings/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from . import views
 
 user_patterns = [
-    path('<uuid:uuid>', views.user_settings),
+    path('<uuid:uuid>', views.user_settings, name="user_settings"),
     path('new', views.create_new_user),
 ]
 
