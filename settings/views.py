bfd30aafbe1c1e675478e4b7baf9d32b209d57f6
snchildress <steven.n.childress@gmail.com>
2018-03-10 20:42:22 -0600

    Create users and profiles on form submission

--- settings/views.py
+++ settings/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 def user_settings(request, id):
@@ -12,13 +13,28 @@ def create_new_user(request):
         return render(request, 'settings/user_settings.html', context)
 
     else:
-        data = request.POST
-        first_name = data['first-name']
-        last_name = data['last-name']
-        username = data['username']
-        password = data['password']
-        annual_accrual_days = data['annual-accrual-days']
-        max_allowable_accrual_days = data['max-allowable-accrual-days']
+        try:
+            # Get form field submissions
+            data = request.POST
+            first_name = data['first-name']
+            last_name = data['last-name']
+            username = data['username']
+            password = data['password']
+            annual_accrual_days = data['annual-accrual-days']
+            max_allowable_accrual_days = data['max-allowable-accrual-days']
+
+            # Create user object
+            user = User.objects.create_user(username, username, password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.profile.annual_accrual_days = annual_accrual_days
+            user.profile.max_allowable_accrual_days = max_allowable_accrual_days
+            user.profile.remaining_accrual_days = 0
+            user.save()
+
+        except Exception as e:
+            print(e)
+
         return render(request, 'settings/user_settings.html', context)
 
 def all_users_settings(request):
