302782b02478c273186692ec1661c7a4e4518fa7
snchildress <steven.n.childress@gmail.com>
2018-03-10 20:15:57 -0600

    Get form field values for new user submission

--- settings/views.py
+++ settings/views.py
@@ -7,7 +7,19 @@ def user_settings(request, id):
 
 def create_new_user(request):
     context = {'new_user': True}
-    return render(request, 'settings/user_settings.html', context)
+
+    if request.method == 'GET':
+        return render(request, 'settings/user_settings.html', context)
+
+    else:
+        data = request.POST
+        first_name = data['first-name']
+        last_name = data['last-name']
+        username = data['username']
+        password = data['password']
+        annual_accrual_days = data['annual-accrual-days']
+        max_allowable_accrual_days = data['max-allowable-accrual-days']
+        return render(request, 'settings/user_settings.html', context)
 
 def all_users_settings(request):
     return render(request, 'settings/all_users_settings.html')
