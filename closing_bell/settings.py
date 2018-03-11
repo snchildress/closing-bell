0cf44745e98e69dd062893a487303d4c4e491f51
snchildress <steven.n.childress@gmail.com>
2018-03-10 21:30:24 -0600

    Create global static directory for the project

--- closing_bell/settings.py
+++ closing_bell/settings.py
@@ -86,7 +86,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'closing_bell/static')]
 for app in CUSTOM_APPS:
     INSTALLED_APPS.append(app)
     STATICFILES_DIRS.append(os.path.join(BASE_DIR, app + '/static'))
