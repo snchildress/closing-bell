ee27b292a40a987279ff8b079f1310c05e0f66aa
snchildress <steven.n.childress@gmail.com>
2018-03-18 23:09:33 -0500

    Auto calculate the number of days requested from the given dates on save

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
-
-from datetime import datetime
 
 
 class Request(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    number_of_days = models.DecimalField(max_digits=4, decimal_places=2,
-                                         default=0)
+    number_of_days = models.DecimalField(max_digits=4, decimal_places=2)
     start_date = models.DateTimeField('Request Start Date')
     end_date = models.DateTimeField('Request End Date')
     create_timestamp = models.DateTimeField('Creation Date',
@@ -19,25 +16,23 @@ class Request(models.Model):
     
     def __str__(self):
         return self.user.first_name + ' ' + \
-            self.user.last_name + '(' + str(self.start_date) + ')'
+            self.user.last_name + ' (' + str(self.start_date) + ')'
 
     class Meta:
         db_table = 'requests'
         verbose_name_plural = 'Vacation Requests'
 
-@receiver(post_save, sender=User)
-def number_of_days(sender, instance, created, **kwargs):
+@receiver(pre_save, sender=Request)
+def number_of_days(sender, instance, **kwargs):
     """
     Uses the start and end dates to save the Request record's
     number of days requested
     """
-    if created:
-        # Convert dates to datetime formats
-        instance.start_date = datetime.strptime(start_date, '%Y-%m-%dT00:00:000Z')
-        instance.end_date = datetime.strptime(end_date, '%Y-%m-%dT00:00:000Z')
+    # Get the record's start and end dates
+    start_date = instance.start_date
+    end_date = instance.end_date
 
-        # Get difference between dates in days, adding 1 to include both dates
-        number_of_days_requested = end_date - start_date
-        number_of_days_requested = number_of_days_requested.days + 1
-        instance.number_of_days = number_of_days_requested
-        instance.save()
+    # Get difference between dates in days, adding 1 to include both dates
+    number_of_days_requested = end_date - start_date
+    number_of_days_requested = number_of_days_requested.days + 1
+    instance.number_of_days = number_of_days_requested
