0ec39220be890293952fb6674ec1c96f1fb541cf
snchildress <steven.n.childress@gmail.com>
2018-03-25 23:01:33 -0500

    Add button to navigate to user's requests as staff user

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.request_vacation, name='request_vacation'),
-    path('<uuid:uuid>', views.request_vacation, name='request_vacation_for_user'),
+    path('<uuid:uuid>', views.request_vacation, name='user_requests'),
     path('delete/request/<int:request_id>', views.delete_request, name='delete_request'),
 ]
